1af11eb50434b779260a7ad6277a4fd55a4e14f3
Abo <252841200@qq.com>
2021-07-19 15:46:20 +0800

    project init (item,order)

---
+++ sp03-userservice/src/main/java/com/abo/jt/Sp03UserserviceApplication.java
@@ -0,0 +1,16 @@
+package com.abo.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Abo
+ */
+@SpringBootApplication
+public class Sp03UserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp03UserserviceApplication.class, args);
+    }
+
+}
