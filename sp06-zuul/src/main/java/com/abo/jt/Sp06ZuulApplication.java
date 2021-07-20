c371faaa291dc51876f1b91ae14dfcea1d0d2380
Abo <252841200@qq.com>
2021-07-20 16:58:25 +0800

    zuul init

---
+++ sp06-zuul/src/main/java/com/abo/jt/Sp06ZuulApplication.java
@@ -0,0 +1,13 @@
+package com.abo.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp06ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp06ZuulApplication.class, args);
+    }
+
+}
---
+++ sp06-zuul/src/test/java/com/abo/jt/Sp06ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abo.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp06ZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
