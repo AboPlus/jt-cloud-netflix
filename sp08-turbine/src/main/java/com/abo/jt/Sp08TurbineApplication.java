037566b85941ce5948334c85d1f8d291f4578d7a
Abo <252841200@qq.com>
2021-07-22 11:48:50 +0800

    turbine

---
+++ sp08-turbine/src/main/java/com/abo/jt/Sp08TurbineApplication.java
@@ -0,0 +1,18 @@
+package com.abo.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author Abo
+ */
+@EnableTurbine
+@SpringBootApplication
+public class Sp08TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp08TurbineApplication.class, args);
+    }
+
+}
